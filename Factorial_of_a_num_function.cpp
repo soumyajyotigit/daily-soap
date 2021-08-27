c6f8bd8d8174b16b77ed0d7384121c2008787ed5
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2021-08-27 12:31:19 +0530

    Create Factorial_of_a_num_function.cpp

---
+++ Factorial_of_a_num_function.cpp
@@ -0,0 +1,22 @@
+//factorial of a num using function 
+//func is like tahtwith args &with return values
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int fact(int n){
+  int factorial=1;
+  for(int i=2;i<=n;i++){
+    factorial=factorial*i;
+  }
+  return factorial;
+}
+int main()
+{
+  int n;
+  cin>>n;
+  int ans=fact(n);
+  cout<<ans<<endl;
+  return 0;
+}
