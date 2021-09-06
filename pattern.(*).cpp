862100525f6efe9a3dc0ecf774892ccdd7d623f8
Soumyajyoti Karmakar <ksoumyajyoti@gmail.com>
2021-09-06 22:47:39 +0530

    Create pattern.(*).cpp

---
+++ pattern.(*).cpp
@@ -0,0 +1,18 @@
+//pattern is like that
+//*******
+//*****
+//***
+//*
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=4;i>=1;i--){
+        for(int j=1;j<=(2*i-1);j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+    return 0;
+}
